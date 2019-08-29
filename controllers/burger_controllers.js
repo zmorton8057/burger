a02150ea2f258ab458cecb5b5b6f46f624c5bc45
Zac Morton <zmorton8057@gmail.com>
2019-08-28 17:40:16 -0700

    Handlebars rendering correctly in the DOM with html templating

--- controllers/burger_controllers.js
+++ controllers/burger_controllers.js
@@ -13,32 +13,29 @@ router.get("/", function (req, res) {
     })
 })
 
-router.get("/burger", function (req, res) {
-    burger.selectAll(function (data) {
-        var burgerObj = {
-            burger: data
-        }
-        console.log(burgerObj);
-        res.render("index", burgerObj)
-    })
-});
-
 router.post("/burger/create", function (req, res) {
     burger.insertOne([
         "burger_name"
     ], [
-            req.body.burger_name],
-        function (data) {
-            res.redirect('/burger')
+            req.body.burger_name
+        ], function (res) {
+            res.json({id: res.insertId})
         });
 });
 
 router.put("/burger/update/:id", function (req, res) {
     var condition = 'id = ' + req.params.id;
+
+    console.log("condition ", condition)
+
     burgers.updateOne({
         "devoured": req.body.devoured
-    }, condition, function (data) {
-        res.redirect("/burger")
+    }, condition, function (result) {
+        if (result.changedRows == 0) {
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
     })
 })
 
